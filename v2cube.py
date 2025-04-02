04878f2d81399945ca28511af4bc07361fec3e75
Roshaan Ali <roshaanalimeher@gmail.com>
2025-04-02 13:01:29 +0100

    update to cube.py, given by a friend's friend.

--- v2cube.py
+++ v2cube.py
@@ -1,6 +1,5 @@
 # variables for the up layer
 u0 = u1 = u2 = u3 = u4 = u5 = u6 = u7 = u8 = None
-b0 = b1 = b2 = b3 = b4 = b5 = b6 = b7 = b8 = None
 up = [
     [u0, u1, u2],
     [u3, u4, u5],
@@ -11,39 +10,39 @@ up = [
 # variables for the front layer
 f0 = f1 = f2 = f3 = f4 = f5 = f6 = f7 = f8 = None
 front = [
-    [u0, u1, u2],
-    [u3, u4, u5],
-    [u6, u7, u8]
+    [f0, f1, f2],
+    [f3, f4, f5],
+    [f6, f7, f8]
 ]
 
 # variables for the right layer
 r0 = r1 = r2 = r3 = r4 = r5 = r6 = r7 = r8 = None
 right = [
-    [u0, u1, u2],
-    [u3, u4, u5],
-    [u6, u7, u8]
+    [r0, r1, r2],
+    [r3, r4, r5],
+    [r6, r7, r8]
 ]
 
 # variables for the down layer
 d0 = d1 = d2 = d3 = d4 = d5 = d6 = d7 = d8 = None
 down = [
-    [u0, u1, u2],
-    [u3, u4, u5],
-    [u6, u7, u8]
+    [d0, d1, d2],
+    [d3, d4, d5],
+    [d6, d7, d8]
 ]
 
 # variables for the left layer
 l0 = l1 = l2 = l3 = l4 = l5 = l6 = l7 = l8 = None
 left = [
-    [u0, u1, u2],
-    [u3, u4, u5],
-    [u6, u7, u8]
+    [l0, l1, l2],
+    [l3, l4, l5],
+    [l6, l7, l8]
 ]
 
 # variables for the back layer
-f0 = f1 = f2 = f3 = f4 = f5 = f6 = f7 = f8 = None
+b0 = b1 = b2 = b3 = b4 = b5 = b6 = b7 = b8 = None
 back = [
-    [u0, u1, u2],
-    [u3, u4, u5],
-    [u6, u7, u8]
+    [b0, b1, b2],
+    [b3, b4, b5],
+    [b6, b7, b8]
 ]
