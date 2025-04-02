03950453b88a26ac7dc2c1d26c0d6386c83b77ab
Roshaan Ali <roshaanalimeher@gmail.com>
2025-04-02 17:56:33 +0100

    used ai to create a script. need to fast track things.

--- v2cube.py
+++ v2cube.py
@@ -16,12 +16,12 @@ def mapColorToFace(faceArr, faceString):
         faceArr[i] = faceString[i]
 
 # 3) Map each face to the desired 9-character color string
-mapColorToFace(u, "wwwwwwwww")
-mapColorToFace(r, "rrrrrrrrr")
-mapColorToFace(f, "ggggggggg")
-mapColorToFace(d, "yyyyyyyyy")
-mapColorToFace(l, "lllllllll")
-mapColorToFace(b, "bbbbbbbbb")
+mapColorToFace(u, "012345678")
+mapColorToFace(r, "012345678")
+mapColorToFace(f, "012345678")
+mapColorToFace(d, "012345678")
+mapColorToFace(l, "012345678")
+mapColorToFace(b, "012345678")
 
 # 4) Test printing a specific sticker (u[1]) or the entire face array
 # 8 corners
@@ -32,13 +32,20 @@ urf = u[8] + r[0] + f[2]
 dlf = d[0] + l[8] + f[6]
 drf = d[2] + r[6] + f[8]
 dlb = d[6] + l[7] + b[8]
-drb = d[8] + r[8] + d[6]
+drb = d[8] + r[8] + b[6]
 
-print(ulb)
-print(urb)
-print(ulf)
-print(urf)
-print(dlf)
-print(drf)
-print(dlb)
-print(drb)
+# adding edges
+ub = u[1] + b[1]
+ur = u[5] + r[1]
+uf = u[7] + f[1]
+ul = u[3] + l[1]
+lf = l[5] + f[3]
+rf = r[3] + f[5]
+rb = r[5] + b[3]
+lb = l[3] + b[5]
+df = d[1] + f[7]
+dr = d[5] + r[7]
+db = d[7] + b[7]
+dl = d[3] + l[7]
+
+print(ub, ur, uf, ul, lf, rf, rb, lb, df, dr, db, dl)
