a3f97f935c5bf80cd661054911c47438bd7e1bae
Roshaan Ali <roshaanalimeher@gmail.com>
2025-04-02 13:43:31 +0100

    created a dictionary of arrays

--- v2cube.py
+++ v2cube.py
@@ -1,48 +1,57 @@
-# variables for the up layer
-u0 = u1 = u2 = u3 = u4 = u5 = u6 = u7 = u8 = None
-up = [
-    [u0, u1, u2],
-    [u3, u4, u5],
-    [u6, u7, u8]
-]
+# # variables for the up layer
+# u0 = u1 = u2 = u3 = u4 = u5 = u6 = u7 = u8 = None
+# up = [
+#     [u0, u1, u2],
+#     [u3, u4, u5],
+#     [u6, u7, u8]
+# ]
 
 
-# variables for the front layer
-f0 = f1 = f2 = f3 = f4 = f5 = f6 = f7 = f8 = None
-front = [
-    [f0, f1, f2],
-    [f3, f4, f5],
-    [f6, f7, f8]
-]
+# # variables for the front layer
+# f0 = f1 = f2 = f3 = f4 = f5 = f6 = f7 = f8 = None
+# front = [
+#     [f0, f1, f2],
+#     [f3, f4, f5],
+#     [f6, f7, f8]
+# ]
 
-# variables for the right layer
-r0 = r1 = r2 = r3 = r4 = r5 = r6 = r7 = r8 = None
-right = [
-    [r0, r1, r2],
-    [r3, r4, r5],
-    [r6, r7, r8]
-]
+# # variables for the right layer
+# r0 = r1 = r2 = r3 = r4 = r5 = r6 = r7 = r8 = None
+# right = [
+#     [r0, r1, r2],
+#     [r3, r4, r5],
+#     [r6, r7, r8]
+# ]
 
-# variables for the down layer
-d0 = d1 = d2 = d3 = d4 = d5 = d6 = d7 = d8 = None
-down = [
-    [d0, d1, d2],
-    [d3, d4, d5],
-    [d6, d7, d8]
-]
+# # variables for the down layer
+# d0 = d1 = d2 = d3 = d4 = d5 = d6 = d7 = d8 = None
+# down = [
+#     [d0, d1, d2],
+#     [d3, d4, d5],
+#     [d6, d7, d8]
+# ]
 
-# variables for the left layer
-l0 = l1 = l2 = l3 = l4 = l5 = l6 = l7 = l8 = None
-left = [
-    [l0, l1, l2],
-    [l3, l4, l5],
-    [l6, l7, l8]
-]
+# # variables for the left layer
+# l0 = l1 = l2 = l3 = l4 = l5 = l6 = l7 = l8 = None
+# left = [
+#     [l0, l1, l2],
+#     [l3, l4, l5],
+#     [l6, l7, l8]
+# ]
 
-# variables for the back layer
-b0 = b1 = b2 = b3 = b4 = b5 = b6 = b7 = b8 = None
-back = [
-    [b0, b1, b2],
-    [b3, b4, b5],
-    [b6, b7, b8]
-]
+# # variables for the back layer
+# b0 = b1 = b2 = b3 = b4 = b5 = b6 = b7 = b8 = None
+# back = [
+#     [b0, b1, b2],
+#     [b3, b4, b5],
+#     [b6, b7, b8]
+# ]
+
+faces = {
+    "up": [[None, None, None], [None, None, None], [None, None, None]],
+    "front": [[None, None, None], [None, None, None], [None, None, None]],
+    "right": [[None, None, None], [None, None, None], [None, None, None]],
+    "down": [[None, None, None], [None, None, None], [None, None, None]],
+    "left": [[None, None, None], [None, None, None], [None, None, None]],
+    "back": [[None, None, None], [None, None, None], [None, None, None]],
+}
