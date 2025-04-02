041d345ba4ed78b01ff461eee3cecbb7276569d5
Roshaan Ali <roshaanalimeher@gmail.com>
2025-04-02 14:21:40 +0100

    reintroduced variables so that it will be easier for me to implement them

--- v2cube.py
+++ v2cube.py
@@ -1,59 +1,59 @@
 # # variables for the up layer
-# u0 = u1 = u2 = u3 = u4 = u5 = u6 = u7 = u8 = None
+u0 = u1 = u2 = u3 = u4 = u5 = u6 = u7 = u8 = None
 # up = [
-#     [u0, u1, u2],
-#     [u3, u4, u5],
-#     [u6, u7, u8]
+    # [u0, u1, u2],
+    # [u3, u4, u5],
+    # [u6, u7, u8]
 # ]
 
 
 # # variables for the front layer
-# f0 = f1 = f2 = f3 = f4 = f5 = f6 = f7 = f8 = None
+f0 = f1 = f2 = f3 = f4 = f5 = f6 = f7 = f8 = None
 # front = [
-#     [f0, f1, f2],
-#     [f3, f4, f5],
-#     [f6, f7, f8]
+    # [f0, f1, f2],
+    # [f3, f4, f5],
+    # [f6, f7, f8]
 # ]
 
 # # variables for the right layer
-# r0 = r1 = r2 = r3 = r4 = r5 = r6 = r7 = r8 = None
+r0 = r1 = r2 = r3 = r4 = r5 = r6 = r7 = r8 = None
 # right = [
-#     [r0, r1, r2],
-#     [r3, r4, r5],
-#     [r6, r7, r8]
+    # [r0, r1, r2],
+    # [r3, r4, r5],
+    # [r6, r7, r8]
 # ]
 
 # # variables for the down layer
-# d0 = d1 = d2 = d3 = d4 = d5 = d6 = d7 = d8 = None
+d0 = d1 = d2 = d3 = d4 = d5 = d6 = d7 = d8 = None
 # down = [
-#     [d0, d1, d2],
-#     [d3, d4, d5],
-#     [d6, d7, d8]
+    # [d0, d1, d2],
+    # [d3, d4, d5],
+    # [d6, d7, d8]
 # ]
 
 # # variables for the left layer
-# l0 = l1 = l2 = l3 = l4 = l5 = l6 = l7 = l8 = None
+l0 = l1 = l2 = l3 = l4 = l5 = l6 = l7 = l8 = None
 # left = [
-#     [l0, l1, l2],
-#     [l3, l4, l5],
-#     [l6, l7, l8]
+    # [l0, l1, l2],
+    # [l3, l4, l5],
+    # [l6, l7, l8]
 # ]
 
 # # variables for the back layer
-# b0 = b1 = b2 = b3 = b4 = b5 = b6 = b7 = b8 = None
+b0 = b1 = b2 = b3 = b4 = b5 = b6 = b7 = b8 = None
 # back = [
-#     [b0, b1, b2],
-#     [b3, b4, b5],
-#     [b6, b7, b8]
+    # [b0, b1, b2],
+    # [b3, b4, b5],
+    # [b6, b7, b8]
 # ]
 
 faces = {
-    "up": [[None, None, None], [None, None, None], [None, None, None]],
-    "front": [[None, None, None], [None, None, None], [None, None, None]],
-    "right": [[None, None, None], [None, None, None], [None, None, None]],
-    "down": [[None, None, None], [None, None, None], [None, None, None]],
-    "left": [[None, None, None], [None, None, None], [None, None, None]],
-    "back": [[None, None, None], [None, None, None], [None, None, None]],
+    "up": [[u0, u1, u2],[u3, u4, u5],[u6, u7, u8]],
+    "front": [[f0, f1, f2],[f3, f4, f5],[f6, f7, f8]],
+    "right": [[r0, r1, r2],[r3, r4, r5],[r6, r7, r8]],
+    "down": [[d0, d1, d2],[d3, d4, d5],[d6, d7, d8]],
+    "left": [[l0, l1, l2],[l3, l4, l5],[l6, l7, l8]],
+    "back": [[b0, b1, b2],[b3, b4, b5],[b6, b7, b8]],
 }
 # will implement this later but for the time being i will let it be.
 # faceColors = {
@@ -73,3 +73,14 @@ def mapColorToFaces(faceName, faceString):
         [faceName[3], faceName[4], faceName[5]],
         [faceName[6], faceName[7], faceName[8]]
     ]
+    
+    
+ 
+mapColorToFaces("up", "yyyyyyyyy")
+mapColorToFaces("right", "rrrrrrrrr")
+mapColorToFaces("front", "ggggggggg")
+mapColorToFaces("down", "ddddddddd")
+mapColorToFaces("left", "lllllllll")
+mapColorToFaces("back", "bbbbbbbbb")
+
+
