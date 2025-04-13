4bb45482721f5d7668d84154afcf1416b23ce00b
Roshaan Ali <roshaanalimeher@gmail.com>
2025-04-13 13:53:44 +0100

    created layout.tsx

---
+++ imagebased/frontend/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Rubik's Solver",
+  description: "Solve a Rubik's Cube from images",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
